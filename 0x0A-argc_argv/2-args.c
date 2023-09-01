938facfdd162e7ada95071dfc84faa7c47c19a5f
root <you@example.com>
2023-09-01 05:51:08 -0700

    a

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -3,7 +3,7 @@
  * main - Write a program that prints all arguments it receives
  *        All arguments should be printed, including the first on
  *
- * @argc: This is the argument count 
+ * @argc: This is the argument count
  * @argv: This is the argument vector
  *
  * Return: 0;
@@ -15,6 +15,5 @@ int main(int argc, char *argv[])
 	for (count = 0; count < argc; count++)
 	{
 		printf("%s\n", argv[count]);
-	}
-	return (0);
+	}	return (0);
 }
